e61c933652b1a0f8d3b7f54d5a3a900b7ed0dc40
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2021-04-01 13:36:05 +0200

    Fix python version to python 3.8.
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- buster_deps.sh
+++ buster_deps.sh
@@ -8,11 +8,11 @@ sha256_file=dd6dc090b6e2580206c64bcee499bc16509a5d017c6952dcd2bed9072af67cbd
 
 sudo apt-get -yqq update
 #sudo apt-get -yqq upgrade
-sudo apt-get -yqq install python3 python3-{pip,virtualenv,mako,dev} \
+sudo apt-get -yqq install python3 python3.8-{pip,virtualenv,mako,dev} \
      curl build-essential libtool autotools-dev automake \
      pkg-config bsdmainutils unzip git gettext \
      build-essential libssl-dev libffi-dev \
-     libxml2-dev libxslt1-dev zlib1g-dev libpython3.8-dev
+     libxml2-dev libxslt1-dev zlib1g-dev libpython3.8-dev python3.8-dev
 
 python3 -m pip install virtualenv
 
